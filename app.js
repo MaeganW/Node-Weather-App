69c57466abe9af527ef1c167039f879e1c857202
MaeganW <maeganwomble@gmail.com>
2018-03-21 14:36:17 -0500

    extracted geocoding function to separate util folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./util/geocode');
 
 const argv = yargs
   .options({
@@ -14,19 +14,4 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-const address = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCpn6XdMEP2oCEAuzp8-S-CdOLxCqwwOM8`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  }
-});
+geocode.geocodeAddress(argv.address);
