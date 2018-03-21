aa18441765341fbf7b3236fe2503f1ee7dea82fb
MaeganW <maeganwomble@gmail.com>
2018-03-21 13:58:40 -0500

    error handling

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCpn6XdMEP2oCEAuzp8-S-CdOLxCqwwOM8`,
   json: true
 }, (error, response, body) => {
-  //this pretty prints
-  // console.log(JSON.stringify(body, undefined, 2));
-
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
