f84f3ac88112391664d85f000b622071ee9bebe6
MaeganW <maeganwomble@gmail.com>
2018-03-21 15:07:09 -0500

    fetching weather from the api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const request = require('request');
 const geocode = require('./util/geocode');
 
 const argv = yargs
@@ -19,5 +20,19 @@ geocode.geocodeAddress(argv.address, (err, res) => {
     console.log(err);
   } else {
     console.log(JSON.stringify(res, undefined, 2));
+    const lat = res.latitude;
+    const lng = res.longitude;
+
+    getWeather(lat, lng);
   }
 });
+
+const getWeather = (lat, lng) => {
+  const url = `https://api.darksky.net/forecast/1c80bf92aadd3db44c05ce1c48912649/${lat},${lng}`;
+  request({
+    url,
+    json: true
+  }, (err, res, body) => {
+    console.log(body.currently.temperature);
+  })
+}
