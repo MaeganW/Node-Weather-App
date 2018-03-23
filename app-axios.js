632ef697a742a6e0358fa99e72696b1954cec941
MaeganW <maeganwomble@gmail.com>
2018-03-23 11:12:22 -0500

    completedaxios version of node-weather-app

--- app-axios.js
+++ app-axios.js
@@ -21,7 +21,15 @@ axios.get(geocodeUrl).then((res) => {
   if(res.data.status === 'ZERO_RESULTS') {
     throw new Error('Unable to find that address');
   }
-  console.log(res.data);
+  const lat = res.data.results[0].geometry.location.lat;
+  const lng = res.data.results[0].geometry.location.lng;
+  const weatherUrl = `https://api.darksky.net/forecast/1c80bf92aadd3db44c05ce1c48912649/${lat},${lng}`;
+  console.log(res.data.results[0].formatted_address);
+  return axios.get(weatherUrl);
+}).then(res => {
+  const temperature = res.data.currently.temperature;
+  const apparentTemperature = res.data.currently.apparentTemperature;
+  console.log(`Current temperature is ${temperature}, and it feels like ${apparentTemperature}`);
 }).catch((err) => {
   if(err.code === 'ENOTFOUND') {
     console.log('Unable to connect to API servers.');
