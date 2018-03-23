103b48b75fec8090161cc4829ec5dae85cdcaa5f
MaeganW <maeganwomble@gmail.com>
2018-03-23 09:08:47 -0500

    successfully converted getWeather to a promise and chained the two promises together

--- app.js
+++ app.js
@@ -15,16 +15,28 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
+// Using Callback on getWeather and a Promise on geocodeAddress
+// geocode.geocodeAddress(argv.address).then((res) => {
+//   const lat = res.latitude;
+//   const lng = res.longitude;
+//   weather.getWeather(lat, lng, (err, res) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(`Current temperature is ${res.temperature}, and it feels like ${res.apparentTemperature}`);
+//     }
+//   });
+// }, err => {
+//   console.log(err);
+// });
+
+// Chaining promises
 geocode.geocodeAddress(argv.address).then((res) => {
   const lat = res.latitude;
   const lng = res.longitude;
-  weather.getWeather(lat, lng, (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`Current temperature is ${res.temperature}, and it feels like ${res.apparentTemperature}`);
-    }
-  });
-}, err => {
+  return weather.getWeather(lat, lng);
+}).then((res) => {
+  console.log(`Current temperature is ${res.temperature}, and it feels like ${res.apparentTemperature}`);
+}).catch(err => {
   console.log(err);
 });
--- util/weather.js
+++ util/weather.js
@@ -1,20 +1,39 @@
 const request = require('request');
 
-const getWeather = (lat, lng, callback) => {
-  const url = `https://api.darksky.net/forecast/1c80bf92aadd3db44c05ce1c48912649/${lat},${lng}`;
-  request({
-    url,
-    json: true
-  }, (err, res, body) => {
-    if (!err && res.statusCode === 200) {
-      callback(undefined, {
-        temperature: body.currently.temperature,
-        apparentTemperature: body.currently.apparentTemperature
-      });
-    } else {
-      callback('Unable to connect to servers.');
-    }
-  })
+// const getWeather = (lat, lng, callback) => {
+//   const url = `https://api.darksky.net/forecast/1c80bf92aadd3db44c05ce1c48912649/${lat},${lng}`;
+//   request({
+//     url,
+//     json: true
+//   }, (err, res, body) => {
+//     if (!err && res.statusCode === 200) {
+//       callback(undefined, {
+//         temperature: body.currently.temperature,
+//         apparentTemperature: body.currently.apparentTemperature
+//       });
+//     } else {
+//       callback('Unable to connect to servers.');
+//     }
+//   })
+// }
+
+const getWeather = (lat, lng) => {
+  return new Promise((resolve, reject) => {
+    const url = `https://api.darksky.net/forecast/1c80bf92aadd3db44c05ce1c48912649/${lat},${lng}`;
+    request({
+      url,
+      json: true
+    }, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        resolve({
+          temperature: body.currently.temperature,
+          apparentTemperature: body.currently.apparentTemperature
+        });
+      } else {
+        reject('Unable to connect to servers.');
+      }
+    })
+  });
 }
 
 module.exports = {
